191063e4649bed2759250e3e0df31396f7d9bacc
Jeff Johnson <johnsonjeff@gmail.com>
2014-09-23 08:21:41 -0700

    TestSuite/TestCase is a simple way to run a set of tests against a set of solutions. Great for code interview practice.

---
+++ TestCases/TestCases/TestCases.cpp
@@ -0,0 +1,50 @@
+// TestCases.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "TestSuite.h"
+#include<functional>
+
+
+bool valid(bool value) {
+	return value;
+}
+
+bool invalid(bool value) {
+	return !value;
+}
+
+int multi_input_valid(int a, int b) {
+	return a + b;
+}
+
+int multi_input_invalid(int a, int b) {
+	return a + b + 1;
+}
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	TestSuite<bool, bool> runner("Simple Input Runner");
+
+	runner.AddSolution("Valid Solution", valid);
+	runner.AddSolution("Invalid Solution", invalid);
+
+	runner.AddTestCase(true, true);
+	runner.AddTestCase(false, false);
+
+	runner.Execute();
+
+	TestSuite<int, int, int> multi_input_runner("Multi-Input Runner");
+
+	multi_input_runner.AddSolution("Valid Solution", multi_input_valid);
+	multi_input_runner.AddSolution("Invalid Solution", multi_input_invalid);
+
+	multi_input_runner.AddTestCase(4, 2, 2);
+	multi_input_runner.AddTestCase(8, 4, 4);
+	multi_input_runner.AddTestCase(3, 2, 1);
+
+	multi_input_runner.Execute();
+
+	return 0;
+}
+
---
+++ TestCases/TestCases/TestSuite.h
@@ -0,0 +1,70 @@
+#include<vector>
+#include<map>
+#include<functional>
+#include<iostream>
+#include<string>
+
+
+/********************************************************************
+ * Simple TestSuite. Built to aid in those one-off scratch programs. 
+ * The ones you'd rather have code verify instead of your tired eyes.
+ *
+ * Usage:
+ *	
+ *		Given a function:
+ *			bool is_two(int value) {
+ *				return value == 2;
+ *			}
+ *	
+ *		TestSuite<bool, int> runner;	// Create a new test suite. TestSuite<output_type, input_types...>
+ *		runner.AddTestCase(true, 2);	// Add test case(s). AddTestCase(output_result, inputs...)
+ *		runner.AddTestCase(false, 1);	
+ *		runner.AddSolution("Valid Solution", is_two); // Add solutions
+ *		runner.Execute();				// Run and done. Output in STDOUT
+*********************************************************************/
+template<typename TestResultType, typename ...TestInputTypes>
+class TestSuite {
+	using SolutionType = typename std::function < TestResultType(TestInputTypes...) >;
+public:
+	TestSuite() { }
+	TestSuite(std::string _test_batch_name) : test_batch_name(_test_batch_name) { }
+	void AddSolution(std::string name, SolutionType handle) {
+		solutions.emplace(name, handle);
+	}
+	void AddTestCase(TestResultType result, TestInputTypes... inputs) {
+		// We need to bind the variable input params to a function placeholder that we can later call with the solutions
+		tests.emplace(result, std::bind(&TestSuite::wrap_solution, this, std::placeholders::_1, inputs...));
+	}
+	void Execute() {
+		std::cout << "= Start batch";
+		if (test_batch_name.length())
+			std::cout << ": " << test_batch_name;
+
+		std::cout << "\n";
+
+		for (auto sln = solutions.begin(); sln != solutions.end(); sln++) {
+			std::cout << " - Solution: " << sln->first << "\n";
+			for (auto test = tests.begin(); test != tests.end(); test++) {
+				assert(test->second(sln->second), test->first);
+			}
+			std::cout << "\n - end\n";
+		}
+		std::cout << "= end \n\n";
+	}
+
+private:
+	bool assert(TestResultType actual, TestResultType expected) {
+		if (actual != expected) {
+			std::cout << "\nFAILURE | Actual: " << (actual) << " | Expected: " << expected << "\n";
+			return false;
+		}
+		std::cout << ".";
+		return true;
+	}
+	TestResultType wrap_solution(SolutionType sln, TestInputTypes...inputs) {
+		return std::bind(sln, inputs...)();
+	}
+	std::map<std::string, SolutionType> solutions;
+	std::map<TestResultType, std::function<TestResultType(SolutionType)>> tests;
+	std::string test_batch_name;
+};
