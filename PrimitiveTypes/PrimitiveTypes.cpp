5cd5e809d3c7cdb95e63d7e47df4395717068a22
Jeff Johnson <jeff@classbug.com>
2016-01-03 09:39:00 -0800

    Add the spiral printing

--- PrimitiveTypes/PrimitiveTypes.cpp
+++ PrimitiveTypes/PrimitiveTypes.cpp
@@ -107,30 +107,156 @@ void parity()
 	runner.Execute();
 }
 
-// TODO
-void dutch_flag()
-{
+template<int R, int C>
+std::vector<int> spiral_fsm(int (A)[R][C], int width, int height) {
+	using std::cout;
 	using std::vector;
-	struct {
-		void operator()(vector<int>& values, int pivot_point) {
 
+	int top = 0;
+	int bottom = height - 1;
+	int left = 0;
+	int right = width - 1;
+	
+	int row = 0;
+	int column = 0;
+
+	enum STATES { LEFT, RIGHT, UP, DOWN };
+	STATES state = RIGHT;
+
+	vector<int> result;
+
+	for (int i = 0; i <= (width*height) + 2*(width-1) + 1; i++) {
+		if (state == RIGHT) {
+			if (column == right) {
+				state = DOWN;
+				top += 1;
+			}
+			else {
+				result.emplace_back(A[row][column]);
+				column += 1;
+			}
 		}
-	} sln;
+		else if (state == DOWN) {
+			if (row == bottom) {
+				state = LEFT;
+				right -= 1;
+			}
+			else {
+				result.emplace_back(A[row][column]);
+				row += 1;
+			}
+		}
+		else if (state == LEFT) {
+			if (column == left) {
+				state = UP;
+				bottom -= 1;
+			}
+			else {
+				result.emplace_back(A[row][column]);
+				column -= 1;
+			}
+		}
+		else if (state == UP) {
+			if (row == top) {
+				state = RIGHT; 
+				left += 1;
+			}
+			else {
+				result.emplace_back(A[row][column]);
+				row -= 1;
+			}
+		}
+	}
 
-	vector<int> simple { 1, 2, 3, 4, 5 };
-	vector<int> simple_sln = simple;
+	return result;
+};
+
+template<int RC>
+void spiral_rings(const int (A)[RC][RC]) {
+	for (int i = 0; i < (ceil((RC)/2)); i++) {
+		print_single_ring<RC>(A, i);
+	}
+
+	// For odd sized matricies we need to print the center element
+	// We can't print a ring of a single element 
+	if (RC % 2 == 1) {
+		std::cout << std::to_string(A[(RC-1) / 2][(RC-1) / 2]);
+	}
+	std::cout << "\n";
+};
+
+template<int RC>
+void print_single_ring(const int(A)[RC][RC], int offset) {
+	using std::cout;
+	using std::to_string;
+
+	if (RC % 2 == 1 && floor(RC / 2) == offset - 1) {
+		cout << to_string(A[offset][offset]) << " ";
+		return;
+	}
+
+	// Right
+	// A[offset][offset] -> A[offset][ RC-offset - 1 ]
+	for (int j = offset; j < (RC - offset - 1); j++) {
+		cout << to_string(A[offset][j]) << " ";
+	}
+	// Down
+	// A[offset][RC-offset] -> A[ RC-offset-1 ][RC-offset]
+	for (int i = offset; i < (RC - offset - 1); i++) {
+		cout << to_string(A[i][RC - offset - 1]) << " ";
+	}
+
+	// Left
+	// A[RC-offset][RC-offset] -> A[RC-offset][ offset - 1 ]
+	for (int j = RC - offset - 1; j > offset; j--) {
+		cout << to_string(A[RC - offset - 1][j]) << " ";
+	}
+
+	// Up
+	// A[RC-offset][offset] -> A[offset -1][offset]
+	for (int i = RC - offset - 1; i > offset; i--) {
+		cout << to_string(A[i][offset]) << " ";
+	}
+};
+
+
+
+void spiral() {
+
+	using std::vector;
+	using std::cout;
+
+	int spiral_3x3[3][3] = { { 0, 1, 2 }, { 3, 4, 5 }, { 6, 7, 8 } };
+
+	spiral_rings<3>(spiral_3x3);
 	
-	sln(simple, 3);
+	cout << "\n";
 
+	int spiral_4x4[4][4] = { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 }, { 13, 14, 15, 16 } };
+	spiral_rings<4>(spiral_4x4);
 
+	cout << "\n";
+
+	int spiral_5x5[5][5] = { { 1, 2, 3, 4, 5 }, { 6, 7, 8, 9, 10 }, { 11, 12, 13, 14, 15 }, { 16, 17, 18, 19, 20 }, { 21, 22, 23, 24, 25 } };
+	spiral_rings<5>(spiral_5x5);
+
+	cout << "\n";
 	
+	/*	
+vector<int> spiral_3x3_sln{ 0, 1, 2, 5, 8, 7, 6, 3, 4 };
+
+	TestSuite<vector<int>, int[3][3], int, int> test_3x3("3x3");
+	test_3x3.AddTestCase(spiral_3x3_sln, spiral_3x3, 3, 3);
+	test_3x3.AddSolution("State Macine", spiral_fsm<3,3>);
+
+	test_3x3.Execute();
+	*/
+	///TestSuite<
 }
-
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	// parity()
-
+	spiral();
 	char f;
 	std::cin >> f;
 }
